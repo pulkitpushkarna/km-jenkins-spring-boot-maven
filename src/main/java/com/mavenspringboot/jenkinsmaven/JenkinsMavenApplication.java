28ce69c514d093e1ce8ec85737b12f758ca35c9f
Pulkit <pulkit.pushkarna@tothenew.com>
2022-01-22 11:43:26 +0530

    V3 commit

--- src/main/java/com/mavenspringboot/jenkinsmaven/JenkinsMavenApplication.java
+++ src/main/java/com/mavenspringboot/jenkinsmaven/JenkinsMavenApplication.java
@@ -11,7 +11,7 @@ public class JenkinsMavenApplication {
 
 	@GetMapping("/")
 	String index(){
-		return "Hello Jenkins maven V2";
+		return "Hello Jenkins maven V3";
 	}
 
 	public static void main(String[] args) {
